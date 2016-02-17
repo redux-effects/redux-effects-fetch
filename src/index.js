e197af4c34303c297810d66dcaffb5dad1a96b54
Lee Siong Chan <ahlee2326@me.com>
2016-02-17 09:28:56 +0800

    Add header checking for json-ld

--- src/index.js
+++ src/index.js
@@ -55,6 +55,7 @@ function createErrorResponse (res) {
 function deserialize (res) {
   const header = res.headers.get('Content-Type') || ''
   if (header.indexOf('application/json') > -1) return res.json()
+  if (header.indexOf('application/ld+json') > -1) return res.json()
   if (header.indexOf('application/octet-stream') > -1) return res.arrayBuffer()
   return res.text()
 }
