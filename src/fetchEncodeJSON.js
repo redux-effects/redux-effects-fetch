1ca4d179feb13d1406e4b0ae1c6f6e44189a8770
Andrew Shaffer <andrew@weo.io>
2016-05-06 18:59:18 -0700

    use global fetch; restyle fetchEncodeJSON to match the style of the project; remove the headers check from fetchEncodeJSON

--- src/fetchEncodeJSON.js
+++ src/fetchEncodeJSON.js
@@ -1,12 +1,17 @@
-import {FETCH} from './index';
+/**
+ * Imports
+ */
+
+import {FETCH} from '.'
 
 /**
  * @see https://github.com/lodash/lodash/blob/4.8.0/lodash.js#L10705
  * @see https://lodash.com/docs#isObject
  */
+
 function isObject(value) {
-  const type = typeof value;
-  return !!value && (type == 'object' || type == 'function');
+  const type = typeof value
+  return !!value && (type == 'object' || type == 'function')
 }
 
 /**
@@ -21,16 +26,20 @@ function isObject(value) {
  *
  * Hook into **before** the regular fetch middleware.
  */
-const fetchEncodeJSON = () => next => action =>
-  action.type === FETCH ?
-    next(maybeConvertBodyToJSON(action)) :
-    next(action);
+
+function fetchEncodeJSON () {
+  return next => action => action.type === FETCH
+    ? next(maybeConvertBodyToJSON(action))
+    : next(action)
+}
 
 /**
  * Whether we may convert a request with the given params to JSON.
  */
-const shallConvertToJSON = (params) =>
-      isObject(params.body) && params.headers && params.headers['Content-Type'] === 'application/json';
+
+function shallConvertToJSON (params) {
+  return isObject(params.body)
+}
 
 /**
  * Add an accept header if necessary.
@@ -38,20 +47,30 @@ const shallConvertToJSON = (params) =>
  * Add an `Accept: application/json` header to the given headers but only if they don't already contain an `Accept`
  * header.
  */
-const maybeAddAcceptHeader = (headers) =>
-      headers.hasOwnProperty('Accept') ? headers : {...headers, 'Accept': 'application/json'};
 
-const maybeConvertBodyToJSON = action => {
-  const { payload } = action;
+function maybeAddAcceptHeader (headers) {
+  return headers.hasOwnProperty('Accept')
+    ? headers
+    : {...headers, 'Accept': 'application/json'}
+}
+
+function maybeConvertBodyToJSON (action) {
+  const {payload} = action
+
   if (shallConvertToJSON(payload.params)) {
-    const body = JSON.stringify(payload.params.body);
-    const headers = maybeAddAcceptHeader(payload.params.headers);
-    const params = {...payload.params, body, headers};
-    const result = {...action, payload: {...payload, params: params}};
-    return result;
-  } else {
-    return action;
-  }
-};
+    const body = JSON.stringify(payload.params.body)
+    const headers = maybeAddAcceptHeader(payload.params.headers)
+    const params = {...payload.params, body, headers}
+    const result = {...action, payload: {...payload, params: params}}
 
-export default fetchEncodeJSON;
+    return result
+  } else {
+    return action
+  }
+}
+
+/**
+ * Exports
+ */
+
+export default fetchEncodeJSON
